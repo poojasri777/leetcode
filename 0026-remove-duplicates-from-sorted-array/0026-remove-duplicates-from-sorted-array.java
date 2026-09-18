98adb093466f1d50008094167adf993a847c53b2
poojasri kallaboina <2403a51262@sru.edu.in>
2026-09-18 22:02:00 +0530

    Time: 1 ms (77.12%), Space: 46.5 MB (81.04%) - LeetHub

---
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        int j=1;
+        int n=nums.length;
+        for(int i=1;i<n;i++){
+            if(nums[i]!=nums[i-1]){
+                nums[j]=nums[i];
+                j++;
+            }
+        }
+        if(n==0){
+            return 0;
+        }
+        return j;
+    }
+}
